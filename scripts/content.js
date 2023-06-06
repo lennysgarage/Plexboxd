77a914caf40df35f4df1d4de1e79ef7907fce560
lennysgarage <lennysgarage1@gmail.com>
2023-06-05 21:59:42 -0400

    feat: update styling & optimize to work for local & web plex
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- scripts/content.js
+++ scripts/content.js
@@ -1,30 +1,40 @@
 let lastURL = location.href;
-const ratings = '[data-testid="metadata-ratings"]'
+const ratings = '[data-testid="metadata-ratings"]';
+const preplay = '[data-testid="preplay-thirdTitle"]';
 
 new MutationObserver(() => {
   const URL = location.href;
-  if (document.querySelector(ratings)) {
+  if (document.querySelector(ratings) || document.querySelector(preplay)) {
     if (URL !== lastURL) {
       lastURL = URL;
-      updateRatings();
+      if (document.querySelector(ratings)) {
+        updateRatings("plex");
+      } else {
+        updateRatings("local");
+      }
     }
   }
 }).observe(document, { subtree: true, childList: true });
 
-async function updateRatings() {
-  const originalTitle = document.querySelector('[data-testid=metadata-title]').textContent.toLowerCase();
+async function updateRatings(mode) {
+  let originalTitle;
+  if (mode === "plex") {
+    originalTitle = document.querySelector('[data-testid=metadata-title]').textContent.toLowerCase();
+  } else {
+    originalTitle = document.querySelector('[data-testid="preplay-mainTitle"]').textContent.toLowerCase();
+  }
   const title = parseMovieTitle(originalTitle);
 
   // fetch with year first
-  const titleWithYear = addYear(title);
-  console.log(titleWithYear)
+  let titleWithYear = addYear(title, mode);
+  console.log(titleWithYear);
 
   const movieWithYear = await chrome.runtime.sendMessage({ title: titleWithYear });
-  if (!updateSingleMovieRating(movieWithYear, titleWithYear)) {
+  if (!updateSingleMovieRating(movieWithYear, titleWithYear, mode)) {
     console.log(title)
     const movieWithoutYear = await chrome.runtime.sendMessage({ title: title });
 
-    updateSingleMovieRating(movieWithoutYear, title)
+    updateSingleMovieRating(movieWithoutYear, title, mode)
   }
 }
 
@@ -39,21 +49,27 @@ function parseMovieTitle(title) {
   return title;
 }
 
-function addYear(title) {
-  let year = document.querySelector('[data-testid=metadata-line1]').textContent;
+function addYear(title, mode) {
+  let year;
+  if (mode === "plex") {
+    year = document.querySelector('[data-testid=metadata-line1]').textContent;
+  } else {
+    year = document.querySelector('[data-testid="preplay-mainTitle"]').parentElement.children[1].children[0].textContent;
+  }
   year = year.split("    ");
   if (year.length > 1) {
     year = year[0];
   }
 
   return titleWithYear = title + "-" + year;
+
 }
 
-function updateSingleMovieRating(html, title) {
+function updateSingleMovieRating(html, title, mode) {
   let rating;
   rating = parseHTML(html)
   if (rating) {
-    return addRating(title,rating)
+    return addRating(title, rating, mode)
   }
   return false;
 }
@@ -75,20 +91,34 @@ function parseHTML(html) {
   return rating;
 }
 
-function addRating(title, rating) {
-  const ratings = document.querySelector('[data-testid="metadata-ratings"]');
+function addRating(title, rating, mode) {
+  let ratings;
+  if (mode === "plex") {
+    ratings = document.querySelector('[data-testid="metadata-ratings"]');
+  } else {
+    ratings = document.querySelector('[data-testid="preplay-mainTitle"]').parentElement.children[2];
+  }
   if (ratings) {
+    const span = document.createElement("span");
+    // span.classList.add("rating-container");
+    ratings.appendChild(span);
+    const div = document.createElement("div");
+    div.classList.add('letterboxd-rating-container');
+    span.appendChild(div);
+
+
     const lImg = document.createElement("img");
     lImg.setAttribute('src', "chrome-extension://__MSG_@@ngilbhhfgkjkfllefakjhieneaoknbfn/../images/letterboxd-decal-dots-pos-rgb-500px.png");
-    lImg.setAttribute('height', '16px');
     lImg.setAttribute('width', '16px');
-    ratings.appendChild(lImg)
+    lImg.setAttribute('height', '16px');
+    lImg.classList.add('letterboxd-img');
+    div.appendChild(lImg)
 
-    const div = document.createElement("a");
-    div.setAttribute('href', `https://letterboxd.com/film/${title}`)
-    div.innerText = rating + "/5.00";
-    div.classList.add("letterboxd-rating");
-    ratings.appendChild(div);
+    const a = document.createElement("a");
+    a.setAttribute('href', `https://letterboxd.com/film/${title}`)
+    a.innerText = rating + "/5.00";
+    a.classList.add("letterboxd-rating");
+    div.appendChild(a);
 
     return true
   }
